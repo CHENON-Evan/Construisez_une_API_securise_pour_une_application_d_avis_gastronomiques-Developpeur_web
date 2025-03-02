be2c5c5bd13a1e9405f7628f2553c99e245a4869
CHENON-Evan <evanchenon@protonmail.com>
2025-03-02 01:49:00 +0100

    add readme

---
+++ Chenon_Evan_1_Code/Backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const userRoutes = require("./routes/users");
+const saucesRoutes = require("./routes/sauces");
+const path = require("path");
+const dotenv = require("dotenv");
+
+// Configure the environment variables
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+});
+
+// MiddLeware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+//Database
+const mongoose = require("mongoose");
+
+const password = process.env.DB_PASSWORD;
+const username = process.env.DB_USER;
+const db = process.env.DB_NAME;
+const uri = `mongodb+srv://${username}:${password}@cluster0.cw1n9.mongodb.net/${db}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to Mongo!"))
+  .catch((err) => console.error("Error connecting to Mongo: ", err));
+
+// Routes
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
+
+module.exports = app;
